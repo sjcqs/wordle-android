a0e707bcb4a5d7c7f5b0e01dbcd38052389152d0
sjcqs <satyan@nabla.com>
2022-01-15 20:06:04 +0100

    Fix letters states

--- data/game/public/src/main/kotlin/fr/sjcqs/wordle/data/game/entity/Game.kt
+++ data/game/public/src/main/kotlin/fr/sjcqs/wordle/data/game/entity/Game.kt
@@ -4,16 +4,35 @@ data class Game(
     val word: String,
     val guessesCount: Int,
     val guesses: List<Guess>,
-    val isFinished: Boolean = false
+    val isFinished: Boolean = false,
 ) {
     val isWon: Boolean = word == guesses.lastOrNull()?.word
     val tileLetters: Map<Int, Map<Char, TileState>> = guesses
-        .fold(mutableMapOf()) { tilesState, guess ->
+        .fold(mutableMapOf<Int, MutableMap<Char, TileState>>()) { tilesState, guess ->
             guess.word.onEachIndexed { index, letter ->
                 val tileState = guess.tiles[index]
                 if (tileState != TileState.Present) {
                     tilesState.compute(index) { _, letters ->
-                        letters.orEmpty().plus(letter to tileState)
+                        (letters ?: mutableMapOf()).apply {
+                            put(letter, tileState)
+                        }
+                    }
+                    if (tileState == TileState.Absent &&
+                        tilesState.values.none { it[letter] == TileState.Present }
+                    ) {
+                        tilesState.replaceAll { _, letters ->
+                            letters.apply {
+                                compute(letter) { _, currentTileState ->
+                                    when (currentTileState) {
+                                        TileState.Present -> currentTileState
+                                        TileState.Correct,
+                                        TileState.Absent,
+                                        null,
+                                        -> TileState.Absent
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
